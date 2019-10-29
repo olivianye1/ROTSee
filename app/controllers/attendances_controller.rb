f9d3490d4c7f265cc39bacc5a1657182e0105fe3
Olivia Nye <onye@tulane.edu>
2019-10-29 21:43:15 +0000

    Added and improved flash messages

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,12 +1,17 @@
 class AttendancesController < ApplicationController
     def create
         @attendance = Attendace.new(attendance_params)
+        
+        flash[:notice] = "Attendance successfully recorded for the #{@attendance.eventDate} event."
+    
     end
     
     def destroy
       #@article = Article.find(params[:article_id])
       #@comment = @article.comments.find(params[:id])
       @attendance.destroy
+      
+      flash[:notice] = "Attendance record successfully deleted."
     end
     
     def edit
