d91ad63e5b877d428f2dc125f6a0c17c5366f5f8
Riley Juenemann <rjuenemann@tulane.edu>
2019-10-29 21:51:29 +0000

    Home page and top menu in header

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :attendances
   end
   resources :attendances
-  root 'cadets#index'
+  root 'cadets#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
