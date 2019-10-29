23092ad118c1999fcd7539d8f941ee9d6f2b5d23
Olivia Nye <onye@tulane.edu>
2019-10-29 20:15:54 +0000

    re-migrate events and attendances

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_220738) do
+ActiveRecord::Schema.define(version: 2019_10_29_201247) do
 
   create_table "attendances", force: :cascade do |t|
     t.string "attended"
