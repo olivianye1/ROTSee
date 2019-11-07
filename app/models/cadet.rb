3fa334d5c0b3ae98573956bb80e82fc0f7c26b5c
Riley Juenemann <rjuenemann@tulane.edu>
2019-11-07 20:50:17 +0000

    adding more specific input validation to event and cadet models

--- app/models/cadet.rb
+++ app/models/cadet.rb
@@ -2,8 +2,39 @@ class Cadet < ApplicationRecord
     has_many :attendances, dependent: :destroy
     has_many :events, through: :attendances
     
-    validates :lastName, :firstName, :email, :phoneNumber, :school, :position, :course, :gradYear, :flight, :username, :password, :presence => true
-    validates :username, :uniqueness => { :case_sensitive => false }
+    validates :school, :position, :course, :gradYear, :flight, :presence => true
+    
+    validates :lastName,
+        presence: true,
+        length: {maximum: 50},
+        allow_nil: false
+        
+    validates :firstName,
+        presence: true,
+        length: {maximum: 50},
+        allow_nil: false
+        
+    validates :email,
+        presence: true,
+        length: {maximum: 100},
+        allow_nil: false
+        
+    validates :phoneNumber,
+        presence: true,
+        length: {maximum: 50},
+        allow_nil: false
+        
+    validates :username,
+        presence: true,
+        length: {maximum: 100},
+        allow_nil: false,
+        :uniqueness => { :case_sensitive => false }
+    
+    validates :password,
+        presence: true,
+        length: {maximum: 100},
+        allow_nil: false
+    
     
     include PgSearch
     pg_search_scope :search_by_full_name, against: [:firstName, :lastName],
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,12 @@ class Event < ApplicationRecord
     has_many :cadets, through: :attendances
     accepts_nested_attributes_for :attendances
     
-    validates :eventDate, :primaryType, :subType, :details, :presence => true
+    validates :eventDate, :primaryType, :subType, :presence => true
+    
+    validates :details,
+        presence: true,
+        length: {maximum: 500},
+        allow_nil: false
     
     include PgSearch
     pg_search_scope :search_by_date, against: [:eventDate],
