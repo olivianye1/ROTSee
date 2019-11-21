753ce57782eeab6edfebe7b6e3666b53369ca403
Jessica Hotsko <jhotsko@tulane.edu>
2019-11-21 17:17:47 +0000

    Remove smtp initializer

--- config/initializers/smtp_settings.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :authentication => :plain,
-    :user_name => ENV['jhotsko'],
-    :password => ENV['hWjFu@c5A4dtpTM'],
-    :domain => 'heroku.com'
-}
-ActionMailer::Base.delivery_method = :smtp
