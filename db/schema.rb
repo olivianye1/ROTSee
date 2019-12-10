5a71f64dfbfa15cd335057dd05b23c9f4d7d35c2
Kennedy Dorsey <kadorsey97@gmail.com>
2019-12-10 01:43:11 +0000

    Add articles scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_194120) do
+ActiveRecord::Schema.define(version: 2019_12_10_000804) do
+
+  create_table "articles", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "attendances", force: :cascade do |t|
     t.integer "attended"
