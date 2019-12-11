3bf270697d253043f734335325b8a3569d36ab2d
Riley Juenemann <rjuenemann@tulane.edu>
2019-12-11 02:25:12 +0000

    add pagination to index for events and posts

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.order(date: :desc).page(params[:page]).per_page(15)
   end
 
   # GET /articles/1
