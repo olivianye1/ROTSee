2866466c20f4d86e67cc78292f1a68cd7e0402e0
Kennedy Dorsey <kadorsey97@gmail.com>
2019-12-11 22:12:48 +0000

    Add validation for article params to article model.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,20 @@
 class Article < ApplicationRecord
+  
+  validates :date => true
+    
+  validates :body,
+    presence: true,
+    length: {maximum: 2000},
+    allow_nil: false
+  
+  validates: :title,
+    presence: true,
+    length: {maximum: 250},
+    allow_nil: false
+
+  validates :tag,
+    presence: true,
+    length: {maximum: 50},
+    allow_nil: false
+    
 end
