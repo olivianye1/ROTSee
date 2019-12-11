b8946fef7235d84751f2f34d4c48eb15d39464d8
Kennedy Dorsey <kadorsey97@gmail.com>
2019-12-11 22:50:14 +0000

    Change articles model to require presence

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,20 @@
 class Article < ApplicationRecord
   
-  validates :date, :body, :title, :tag => true
-  
+  validates :title,
+    presence: true,
+    length: {maximum: 256},
+    allow_nil: false
     
+  
+  validates :body,
+    presence: true,
+    length: {maximum: 2000},
+    allow_nil: false
+    
+
+  validates :tag,
+    presence: true,
+    length: {maximum: 50},
+    allow_nil: false
+  
 end
