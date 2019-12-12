474510baaa85540786da25ec0a5a319f92daa33e
Olivianye1 <onye@tulane.edu>
2019-12-12 07:12:16 +0000

    configure local downloads to go through gem

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -61,24 +61,33 @@
 
 if Rails.env.production?
   wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  
+  WickedPdf.config ||= {exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+  WickedPdf.config.merge!({
+   # your extra configurations here
+    layout: 'roster.html.erb',
+    print_media_type: true,
+    page_size: 'A4',
+    encoding: 'utf-8',
+    formats: [:html],
+    zoom: 0.8
+ })
+
 else
   # Linux (check your processor for Intel x86 or AMD x64)
   # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
   # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
   # OS X
-  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-macos_carbon.gz"            
+  WickedPdf.config ||= {
+   # your extra configurations here
+    layout: 'roster.html.erb',
+    print_media_type: true,
+    page_size: 'A4',
+    encoding: 'utf-8',
+    formats: [:html],
+    zoom: 0.8
+  }
   # Windows
   # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
 end
 
-
-WickedPdf.config ||= {exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
-WickedPdf.config.merge!({
-  # your extra configurations here
-   layout: 'roster.html.erb',
-   print_media_type: true,
-   page_size: 'A4',
-   encoding: 'utf-8',
-   formats: [:html],
-   zoom: 0.8
- })
