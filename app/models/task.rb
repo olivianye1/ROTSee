96483eb9cad9561461caea99973d7b4a9d2e7240
Riley Juenemann <rjuenemann@tulane.edu>
2019-12-12 02:41:44 +0000

    input validation size restriction for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
     belongs_to :cadet
     
-    validates :date_created, :date_due, :description, :completed, :cadet_id, :presence => true
+    validates :date_created, :date_due, :completed, :cadet_id, :presence => true
+    
+    validates :descriptiony,
+        presence: true,
+        length: {maximum: 2000},
+        allow_nil: false
 end
